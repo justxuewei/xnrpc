016b8f4e6de1482888ff1b080e49c967068f7120
XavierNiu <a@nxw.name>
2021-05-10 14:54:13 +0800

    day1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xavier-niu/xnrpc
+
+go 1.16
---
+++ pkg/rpc/server.go
@@ -0,0 +1,134 @@
+package rpc
+
+import (
+	"encoding/json"
+	"github.com/xavier-niu/xnrpc/pkg/rpc/codec"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+)
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+// ----- Server -----
+
+var DefaultServer = NewServer()
+
+type Server struct {}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Accept(l net.Listener) {
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go s.ServeConn(conn)
+	}
+}
+
+func (s *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() {
+		_ = conn.Close()
+	}()
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+	s.serveCodec(f(conn))
+}
+
+// TODO: a placeholder
+var invalidRequest = struct{}{}
+
+func (s *Server) serveCodec(cc codec.Codec) {
+	// one response could be sent at the same time
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	// accept multiple requests with one tcp connection
+	for {
+		req, err := s.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			s.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go s.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+type request struct {
+	h *codec.Header
+	argv, replyv reflect.Value
+}
+
+func (s *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (s *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := s.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	// TODO: We don't know the type of request argv at this moment, we just suppose it's a string in day1
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv err:", err)
+	}
+	return req, nil
+}
+
+func (s *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+
+}
+
+func (s *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+
+}
+
+// ----- Public Methods -----
+
+func Accept(l net.Listener) {
+
+}
